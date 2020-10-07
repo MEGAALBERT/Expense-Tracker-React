1980f91537d21a202b6392a1869f098e099cfca3
Alberto Medellin <albertomedellin85@gmail.com>
2020-10-06 23:15:22 -0700

    Added context for global state management

--- src/components/TransactionList.js
+++ src/components/TransactionList.js
@@ -1,13 +1,21 @@
-import React from 'react'
+import React, {useContext} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 function TransactionList() {
+
+    //To bring the context state to the component
+const {transactions} = useContext(GlobalContext);
+
+
     return (
         <>
             <h3>History</h3>
             <ul className='list'>
+                {transactions.map(transaction => (
                 <li className='minus'>
-                    Cash <span>-$400</span><button className='delete-btn'>x</button>
+                {transaction.text} <span>-$400</span><button className='delete-btn'>x</button>
                 </li>
+                ))}
             </ul>
         </>
     )
