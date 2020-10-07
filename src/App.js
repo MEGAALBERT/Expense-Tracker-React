afe9426da43f97c216204a03bc8594b0a572d535
Alberto Medellin <albertomedellin85@gmail.com>
2020-10-06 17:28:38 -0700

    Add Balance and Header component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import Header from './components/Header';
+import Balance from './components/Balance';
+import './App.css'
 
 function App() {
   return (
     <div className="App">
-      HelloWorld
+      <Header/>
+      <div className="container">
+      <Balance/>
+      </div>
     </div>
   );
 }
